65628f1258ffae09c11e1d705189638b6eae7d8a
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-06-30 17:32:20 +0430

    Add the UI design for Calculator

---
+++ src/Component/Calculator.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../index.css';
+
+export default function Calculator() {
+  return (
+    <div className="calc">
+      <div className="calc-style">
+        <div className="row">
+          <button className="btn operator" type="button">AC</button>
+          <button className="btn operator" type="button">+/-</button>
+          <button className="btn operator" type="button">%</button>
+        </div>
+        <div className="row">
+          <button className="btn" type="button">7</button>
+          <button className="btn" type="button">8</button>
+          <button className="btn" type="button">9</button>
+          <button className="btn operator" type="button">/</button>
+        </div>
+        <div className="row">
+          <button className="btn" type="button">4</button>
+          <button className="btn" type="button">5</button>
+          <button className="btn" type="button">6</button>
+          <button className="btn operator" type="button">x</button>
+        </div>
+        <div className="row">
+          <button className="btn" type="button">1</button>
+          <button className="btn" type="button">2</button>
+          <button className="btn" type="button">3</button>
+          <button className="btn operator" type="button">+</button>
+        </div>
+        <div className="row">
+          <button className="btn operator" type="button">.</button>
+          <button className="btn" type="button">0</button>
+          <button className="btn operator" type="button">-</button>
+          <button className="btn operator" type="button">=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
